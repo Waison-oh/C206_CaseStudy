2686483f01be6029340b89e907d80bf4a10458ca
19038285 <19038285@DESKTOP-2TP3HED>
2020-08-23 14:16:09 +0800

    menu class with quick fix applied before menuItem arrayList is done

--- C206_CaseStudy/src/Menu.java
+++ C206_CaseStudy/src/Menu.java
@@ -1,10 +1,13 @@
+import java.awt.MenuItem; //this is quick fix before the menuItem array List is out
 import java.util.ArrayList;
 
+
 public class Menu {
+
 	private String displayName;
 	private int month;
 	private int numberOffers;
-	private ArrayList items;
+	private ArrayList<MenuItem> items;
 	
 	public Menu(String displayName,int month,int numberOffers, ArrayList items) {
 		this.displayName = displayName;
@@ -12,5 +15,38 @@ public class Menu {
 		this.numberOffers = numberOffers;
 		this.items = items;
 	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getNumberOffers() {
+		return numberOffers;
+	}
+
+	public void setNumberOffers(int numberOffers) {
+		this.numberOffers = numberOffers;
+	}
+
+	public ArrayList<MenuItem> getItems() {
+		return items;
+	}
+
+	public void setItems(ArrayList<MenuItem> items) {
+		this.items = items;
+	}
+	
 
 }
