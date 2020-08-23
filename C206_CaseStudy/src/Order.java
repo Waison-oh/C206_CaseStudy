e8d69dddc8d5c2828db6c443e9d87fe822274ab7
19013363 <19013363@LAPTOP-4JSUGDV9>
2020-08-23 12:28:24 +0800

    \\Qi Yue

---
+++ C206_CaseStudy/src/Order.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19013363, 9/6/2020 10:20:32 am
+ */
+
+public class Order {
+
+}
