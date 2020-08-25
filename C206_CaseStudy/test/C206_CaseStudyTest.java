1c9620800f54a0898b8fb963f447e50b478b82c6
19038285 <19038285@DESKTOP-2TP3HED>
2020-08-25 17:35:24 +0800

    test code (1/4 progress)

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,13 +1,29 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	private MenuItem MI1;
+	private MenuItem MI2;
+	private Account A1;
+	private Account A2;
+	private Order order1;
+	private Order order2;
+	
+
+	private ArrayList<Menu> menuList = new ArrayList<Menu>();
+	private ArrayList<Account> accountList = new ArrayList<Account>();
+	private ArrayList<Order> orderList = new ArrayList<Order>();
+	private ArrayList<MenuItem> items = new ArrayList<MenuItem>();
+	private ArrayList<Bill> billList = new ArrayList<Bill>();
 
 	@Before
 	public void setUp() throws Exception {
+		
 	}
 
 	@After
