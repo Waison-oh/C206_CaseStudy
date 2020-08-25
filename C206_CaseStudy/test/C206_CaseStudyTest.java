76ef3db6a16fbbd5a3e5b2f6aebf00b82a690647
19038285 <19038285@DESKTOP-2TP3HED>
2020-08-25 18:00:34 +0800

    update test code

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -40,8 +40,19 @@ public class C206_CaseStudyTest {
 		//fail("Not yet implemented"); 
 		assertTrue("C206_CaseStudy_SampleTest ",true);
 	}
+	
 	@Test
 	public void createMenuTest() {
+		
+	}
+	
+	@Test
+	public void viewMenuTest() {
+		
+	}
+	
+	@Test
+	public void deleteMenuTest() {
 		
 	}
 
