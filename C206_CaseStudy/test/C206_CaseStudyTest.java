2cc0e9ec31fff7201e97921d68799452eb100e9f
19038285 <19038285@DESKTOP-2TP3HED>
2020-08-28 00:52:51 +0800

    push in basic junit test

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 public class C206_CaseStudyTest {
 	
-	ArrayList<Menu> menuList = new ArrayList<Menu>();
+	/* ArrayList<Menu> menuList = new ArrayList<Menu>();
 	ArrayList<Account> accountList = new ArrayList<Account>();
 	ArrayList<Order> orderList = new ArrayList<Order>();
 	ArrayList<MenuItem> items = new ArrayList<MenuItem>();
@@ -248,6 +248,104 @@ public class C206_CaseStudyTest {
 		return output;
 	}
 	// ASHLEIGH ENDS HERE//
+	 
+	 */
+	
+	private MenuItem MI1;
+	private MenuItem MI2;
+	private Account A1;
+	private Account A2;
+	private Order order1;
+	private Order order2;
+
+	private Menu monthMenu;
+	private Menu monthMenu1;
+
+	private ArrayList<Menu> menuList;
+	private ArrayList<Account> accountList;
+	private ArrayList<Order> orderList ;
+	private ArrayList<MenuItem> items ;
+	private ArrayList<Bill> billList ;
+
+	public C206_CaseStudyTest() {
+		super();
+	}
+	@Before
+	public void setUp() throws Exception {
+		//fill in respectively, thanksss
+		MI1 = new MenuItem(null, null, false, 0);
+		MI2 = new MenuItem(null, null, false, 0);
+		//all the arrayList:
+
+		menuList = new ArrayList<Menu>();
+		accountList = new ArrayList<Account>();
+		orderList = new ArrayList<Order>();
+		items = new ArrayList<MenuItem>();
+		billList = new ArrayList<Bill>();
+
+
+		// menu item
+		MI1 = new MenuItem("Western", "Chicken nugget", false, 6);
+		MI2 = new MenuItem("Vegetarian", "Potato", true, 5);
+
+		items.add(MI1);
+		items.add(MI2);
+
+		//monthly menu
+		monthMenu = new Menu("Jan menu", 1, 1, items);
+		monthMenu1 = new Menu("Feb menu", 2, 1, items);
+		menuList.add(monthMenu);
+		menuList.add(monthMenu1);
+
+		//account stuff
+		A1 = new Account(null, null, null, null);
+		A2 = new Account(null, null, null, null);
+
+		menuList= new ArrayList<Menu>();
+		accountList= new ArrayList<Account>();
+		orderList = new ArrayList<Order>();
+		items = new ArrayList<MenuItem>();
+		billList = new ArrayList<Bill>();		
+	}
+
+	@Test
+	public void c206_test() {
+		//fail("Not yet implemented"); 
+		assertTrue("C206_CaseStudy_SampleTest ",true);
+		// fail("Not yet implemented");
+		assertTrue("C206_CaseStudy_SampleTest ", true);
+	}
+	
+
+	@Test
+	public void createMenuTest() {
+		// test if the menu is created - normal
+		C206_CaseStudy.createMenu(menuList, monthMenu);
+		assertSame("Check that menu is create", monthMenu.getDisplayName(), menuList.get(0).getDisplayName());
+
+		//test 
+
+	}
+	
+
+	@Test
+	public void viewMenuTest() {
+		
+
+	}
+	
+
+	@Test
+	public void deleteMenuTest() {
+		
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	
 	
 	}
 
